ac130d8d4a48b230fadeb56e62eaaf61dd258aa5
Andi Heinkelein <andifeind@noname-media.com>
2018-04-25 23:30:48 +0200

    Remove prettier

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-const prettier = require('prettier')
 
 const FireScriptTokenizer = require('./FireScriptTokenizer')
 const FireScriptParser = require('./FireScriptParser')
@@ -40,15 +39,6 @@ module.exports = {
     opts = opts || {}
     const parser = opts.type === 'js' ? new JSParser() : new FireScriptParser()
     return parser.parse(input)
-  },
-  retranspile (input, opts) {
-    return prettier.format('', {
-      parser () {
-        const fsParser = new FireScriptParser(opts)
-        const ast = fsParser.parse(input)
-        return ast
-      }
-    })
   },
   loadConf (customConf) {
     const config = new FSConfig()
