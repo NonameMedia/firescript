2ee59cf8b1537eb825ef3989afa088452eff3afb
Andi Heinkelein <andifeind@noname-media.com>
2018-05-05 13:12:38 +0200

    Unittest EmptyStatement

--- src/fs-elements/EmptyStatement.js
+++ src/fs-elements/EmptyStatement.js
@@ -11,18 +11,8 @@ const FireScriptElement = require('./FireScriptElement')
 }
 */
 class EmptyStatement extends FireScriptElement {
-  constructor (ast) {
-    super(ast)
-
-    this.callee = this.createElement(ast.callee)
-    this.arguments = this.createElementList(ast.arguments)
-    throw new Error(`Element EmptyStatement is a DraftElement!`)
-  }
-
-  toString () {
-    return this.renderElement(
-      `${this.callee}(${this.arguments.join(', ')});`
-    )
+  toFSString () {
+    return ''
   }
 }
 
