94cec3123e98cba7672e9870188a330a95ba639a
Andi Heinkelein <andifeind@noname-media.com>
2018-05-11 17:03:10 +0200

    WIP init.js

--- bin/init.js
+++ bin/init.js
@@ -2,6 +2,7 @@ const path = require('path')
 const SuperFS = require('superfs')
 const superconf = require('superconf')
 const colorfy = require('colorfy')
+const defaultConf = require(path.join(__dirname, '../conf/defaultConf.json'))
 
 module.exports = (supershit) => {
   return supershit
@@ -9,16 +10,14 @@ module.exports = (supershit) => {
     .description('Create and initialize a .firerc configuration file under current working dir')
     .action(async function (ctx, name) {
       const configFile = path.join(process.cwd(), '.firerc')
-      const conf = superconf('fire', {
-        defaultConf: path.join(__dirname, '../conf/defaultConf.json')
-      })
+      const conf = superconf.merge(defaultConf, superconf('fire') || {})
 
       const res = await this
-        .ask({ name: 'src', default: conf.src })
-        .ask({ name: 'dest', default: conf.dest })
-        // .ask({ name: 'copy', default: conf.copy, type: 'array' })
-        .ask({ name: 'esModules', description: 'ES7 modules enabled (y[es] n[o]?', default: conf.features.esModules ? 'yes' : 'no', type: 'bool', allow: ['y', 'yes', 'n', 'no'] })
-        .ask({ name: 'esClasses', description: 'ES6 classes enabled (y[es] n[o]?', default: conf.features.esClasses ? 'yes' : 'yes', type: 'bool', allow: ['y', 'yes', 'n', 'no'] })
+        .ask({ name: 'src', question: 'Source folder', default: conf.src })
+        .ask({ name: 'dest', question: 'Destination folder', default: conf.dest })
+        .ask({ name: 'copy', question: 'Copy file patterns', default: conf.copy, type: 'array' })
+        .ask({ name: 'esModules', question: 'ES7 modules enabled (y[es] n[o]?', default: conf.features.esModules ? 'yes' : 'no', type: 'bool', allow: ['y', 'yes', 'n', 'no'] })
+        .ask({ name: 'esClasses', question: 'ES6 classes enabled (y[es] n[o]?', default: conf.features.esClasses ? 'yes' : 'yes', type: 'bool', allow: ['y', 'yes', 'n', 'no'] })
         .prompt()
 
       console.log(res)
@@ -26,7 +25,7 @@ module.exports = (supershit) => {
       const prettyResult = JSON.stringify({
         src: res.src,
         dest: res.dest,
-        // copy: res.copy,
+        copy: res.copy,
         features: {
           esModules: res.esModules,
           esClasses: res.esClasses
