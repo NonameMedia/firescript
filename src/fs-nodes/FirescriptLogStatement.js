3ebb3228b95a8c094646561775a63e766302deea
Andi Heinkelein <andifeind@noname-media.com>
2018-07-07 19:50:54 +0200

    Add log statement

---
+++ src/fs-nodes/FirescriptLogStatement.js
@@ -0,0 +1,32 @@
+const FirescriptNode = require('./FirescriptNode')
+
+class FirescriptLogStatement extends FirescriptNode {
+  constructor (tokenStack, parent, name) {
+    super(tokenStack, parent)
+
+    if (!tokenStack.expect('keyword', 'log')) {
+      this.syntaxError('Unexpected Token! FirescriptLogStatement expected', tokenStack.current())
+    }
+
+    tokenStack.goForward()
+
+    this.arguments = []
+    while (true) {
+      const expression = this.createFullNode(tokenStack)
+      this.arguments.push(expression)
+
+      if (tokenStack.isIndention('eq', this.indention)) {
+        break
+      }
+    }
+  }
+
+  toJSON () {
+    return this.createJSON({
+      type: 'FirescriptLogStatement',
+      arguments: this.arguments.map((item) => item.toJSON())
+    })
+  }
+}
+
+module.exports = FirescriptLogStatement
