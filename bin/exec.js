c372255100244291465ff65899f9cf1f4062b676
Andi Heinkelein <andifeind@noname-media.com>
2019-11-28 22:32:22 +0100

    Add exec command

--- bin/exec.js
+++ bin/exec.js
@@ -1,13 +1,16 @@
 const path = require('path')
-const SuperFS = require('superfs')
-const superconf = require('superconf')
-const colorfy = require('colorfy')
 
 module.exports = (fireio) => {
   return fireio
     .cmd('exec <file>')
-    .description('Executes a file')
+    .description('Executes a .fire file')
     .action(async (ctx, file) => {
-      console.log('Default command', file)
+      if (path.extname(file) !== '.fire') {
+        throw new Error('Input file seems not to be a .fire file')
+      }
+
+      file = path.resolve(process.cwd(), file)
+      require('../src/register')
+      require(file)
     })
 }
